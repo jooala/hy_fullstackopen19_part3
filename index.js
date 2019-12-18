eada44b78a1545eea484847a1a7702637e80db0b
Joonas Alanenpää <joonas.alanenpaa@gmail.com>
2019-12-18 04:03:13 +0200

    fixed deletion

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ app.get("/api/persons/:id", (req, res) => {
 
 app.delete("/api/persons/:id", (req, res) => {
   const id = Number(req.params.id);
-  const person = persons.find(person => person.id === id);
+  persons = persons.filter(person => person.id !== id);
 
   res.status(204).end();
 });
