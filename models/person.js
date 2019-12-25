ff351d0142efab85775ca8aca45f5e85150e7d66
jooala <joonas.alanenpaa@helsinki.fi>
2019-12-25 03:26:35 +0200

    fixing uri to heroku

--- models/person.js
+++ models/person.js
@@ -2,18 +2,16 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const validator = require('mongoose-unique-validator')
 
-const url = process.env.MONGODB_URI;
-
 console.log("connecting to", url);
 
 mongoose
-  .connect(url, { useNewUrlParser: true })
-  .then(result => {
-    console.log("connected to MongoDB");
+  .connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+  .then(() => {
+    console.log('connected to MongoDB')
   })
   .catch(error => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
   name: {
